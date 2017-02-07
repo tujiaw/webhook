566d4fc6214cb2119ff72b9b00a604e44f9cc172
tujiaw <tujiaw@163.com>
2017-02-07 14:13:08 +0800

    add

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/nodeblog', secret: '123456' })
 
+function RunCmd(cmd, args, cb) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var result = '';
+  child.stdout.on('data', function(data) {
+    result += data.toString();
+  });
+  child.stdout.on('end', function() {
+    cb(result)
+  });
+}
+
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
@@ -17,6 +29,9 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+  RunCmd('sh', './deploy.sh', function(result) {
+      console.log(result);
+  })
 })
 
 handler.on('issues', function (event) {
