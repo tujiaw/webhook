f2c32a2a5072fe1eeb60afa2031f94370d35e91f
tujiaw <tujiaw@163.com>
2017-03-27 14:46:32 +0800

    add

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/nodeblog', secret: '123456' })
 
 function RunCmd(cmd, args, cb) {
   var spawn = require('child_process').spawn;
@@ -13,37 +12,6 @@ function RunCmd(cmd, args, cb) {
     cb(result)
   });
 }
-
-/* 单应用
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404;
-    res.end('no such location');
-  })
-}).listen(3333)
-
-handler.on('error', function (err) {
-  console.error('Error:', err.message);
-})
-
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref);
-  var shpath = './' + event.payload.repository.name + '.sh';
-  RunCmd('sh', [shpath], function(result) {
-      console.log(result);
-  })
-})
-
-handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title);
-})
-*/
 
 /////多应用/////////////////////////////////////
 function generaterHandler(handlerOpts) {
@@ -61,8 +29,6 @@ function generaterHandler(handlerOpts) {
   }).listen(3333)
 }
 
-var http = require('http')
-var createHandler = require('github-webhook-handler')
 var handlerOpts = [{
   path: '/nodeblog',
   secret: '123456'
@@ -70,18 +36,18 @@ var handlerOpts = [{
   path: '/koablogdemo',
   secret: '123456'
 }]
-var handler = generaterHandler(handlerOpts)
 
+var handler = generaterHandler(handlerOpts)
 handler.on('error', function(err) {
   console.error('Error:', err.message)
 })
 
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
-	event.payload.repository.name,
-	event.payload.ref);
+	  event.payload.repository.name,
+	  event.payload.ref);
   var shpath = './' + event.payload.repository.name + '.sh';
   RunCmd('sh', [shpath], function(result) {
       console.log(result);
   })
- )
+})
