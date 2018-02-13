815ddf76dc233e06f2c35d24a7f1fb1c88d0872a
tujiaw <tujiaw@tujiawdeMacBook-Pro.local>
2018-02-13 18:46:33 +0800

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var http = require('http')
 var createHandler = require('node-github-webhook')
 var handler = createHandler([ // multiple handlers
-  { path: '/nodeblog', secret: '123456' },
+  { path: '/3inns', secret: '123456' },
   { path: '/koablogdemo', secret: '123456' },
   { path: '/sanjiadian', secret: '123456' },
   { path: '/NodeChat', secret: '123456' },
